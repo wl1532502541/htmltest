efb7566d6d0cfbd6d20da445067439b5fd7589e5
wl1532502541 <1532502541@qq.com>
2021-12-02 23:58:54 +0800

    fix

--- mini/mini-express/index.js
+++ mini/mini-express/index.js
@@ -3,12 +3,13 @@ const express = require('./mini-express.js');
 const app = new express();
 
 app.get('/test', (req, res, next) => {
-  console.log('你进入了/test的第一个路由');
+  console.log('你进入了/test的第一个中间件');
   next();
 })
 
 app.get('/test', (req, res, next) => {
-  console.log('你进入了/test的第二个路由')
+  console.log('你进入了/test的第二个中间件')
+  res.end('req',req)
 })
 
 app.listen(8888, (err) => {
